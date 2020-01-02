5a1bbeaf60e87dd211585115758e0b1d2ae334c6
aaronbcrampton <aaronbcrampton@gmail.com>
2020-01-02 09:16:23 -0500

    This commit has some unwanted coding at the top.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  FORWARD:  I am also one to avoid single letter variables so I made more descriptive names.  In retrospect it complicated
+##   this particular assignment.  It's still a good habit though in my belief.  Thanks for reviewing and for any feedback!
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(item = matrix()) {   ##creates a matrix
+  inverse <- NULL                                 ##establishes the empty version of the 'inverse' vector
+  set <- function(action){                        ##creates set vector to store the new value of matrix 'item'
+    item <<- action                               ##stores in the global environment
+    inverse <<- NULL                              ##establishes the emply version of the inverse variable in global environment
+  }
+  get <- function() item                           ##defines 'get' as as function on 'item' matrix
+  setinverse <- function(storedinverse) inverse <<- inverse     # stores 'storedinverse' results in parent environment 'inverse'
+  getinverse <- function() inverse                 ## gets the value of 'inverse'
+  list (inverse = inverse, 
+        get = get, 
+        setinverse = setinverse, 
+        getinverse = getinverse)                   ##allows to be called with $
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(item, ...) {              ##establish function for inverse
+  inverse <- item$makeCacheMatrix()
+  if(!is.null(inverse)) {                        ##retrieves cached data if it exists
+    message("getting cashed data")
+    return(m)
+  }
+  data <- item$get()                             ##runs function for inverse if it does not exist in cache
+  inverse <- solve(data, ...)
+  item$setinverse(inverse)
+  inverse
 }
